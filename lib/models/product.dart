2437587a21e102018d5490e79b2a735a3b517a86
Linh Đặng <nhacuaphuong@gmail.com>
2022-10-19 08:55:41 +0700

    Cai dat chuc nang chon san pham yeu thich

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,30 +1,51 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+
 class Product {
   final String? id;
   final String title;
   final String description;
   final double price;
   final String imageUrl;
-  final bool isFavorite;
-  Product(
-      {this.id,
-      required this.title,
-      required this.description,
-      required this.price,
-      required this.imageUrl,
-      this.isFavorite = false});
-  Product copyWith(
-      {String? id,
-      String? title,
-      String? description,
-      double? price,
-      String? imageUrl,
-      bool? isFavorite}) {
+  final ValueNotifier<bool> _isFavorite;
+
+  Product({
+    this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.imageUrl,
+    isFavorite = false,
+  }) : _isFavorite = ValueNotifier(isFavorite);
+
+  set isFavorite(bool newValue) {
+    _isFavorite.value = newValue;
+  }
+
+  bool get isFavorite {
+    return _isFavorite.value;
+  }
+
+  ValueNotifier<bool> get isFavoriteListenable {
+    return _isFavorite;
+  }
+
+  Product copyWith({
+    String? id,
+    String? title,
+    String? description,
+    double? price,
+    String? imageUrl,
+    bool? isFavorite,
+  }) {
     return Product(
-        id: id ?? this.id,
-        title: id ?? this.title,
-        description: description ?? this.description,
-        price: price ?? this.price,
-        imageUrl: imageUrl ?? this.imageUrl,
-        isFavorite: isFavorite ?? this.isFavorite);
+      id: id ?? this.id,
+      title: title ?? this.title,
+      description: description ?? this.description,
+      // ignore: unnecessary_this
+      price: price ?? this.price,
+      imageUrl: imageUrl ?? this.imageUrl,
+      isFavorite: isFavorite ?? this.isFavorite,
+    );
   }
 }
--- lib/ui/products/product_grid_tile.dart
+++ lib/ui/products/product_grid_tile.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../models/product.dart';
-import 'product_detail_screen.dart';
+import './product_detail_screen.dart';
 
 class ProductGridTile extends StatelessWidget {
   const ProductGridTile(this.product, {super.key});
@@ -10,44 +10,51 @@ class ProductGridTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
-        borderRadius: BorderRadius.circular(10),
-        child: GridTile(
-          footer: buildGridFooterBar(context),
-          child: GestureDetector(
-            onTap: () {
-              Navigator.of(context).pushNamed(
-                ProductDetailScreen.routeName,
-                arguments: product.id,
-              );
-            },
-            child: Image.network(
-              product.imageUrl,
-              fit: BoxFit.cover,
-            ),
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        footer: buildGridFooterBar(context),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              ProductDetailScreen.routeName,
+              arguments: product.id,
+            );
+          },
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
           ),
-        ));
+        ),
+      ),
+    );
   }
 
   Widget buildGridFooterBar(BuildContext context) {
     return GridTileBar(
-      backgroundColor: Colors.black87,
-      leading: IconButton(
-        icon: Icon(
-          product.isFavorite ? Icons.favorite : Icons.favorite_border,
-        ),
-        color: Theme.of(context).colorScheme.secondary,
-        onPressed: (() {
-          print('Add item to favorite');
-        }),
-      ),
+      backgroundColor: Colors.black45,
+      leading: ValueListenableBuilder<bool>(
+          valueListenable: product.isFavoriteListenable,
+          builder: (ctx, isFavorite, child) {
+            return IconButton(
+              icon: Icon(
+                product.isFavorite ? Icons.favorite : Icons.favorite_border,
+              ),
+              color: Theme.of(context).colorScheme.secondary,
+              onPressed: () {
+                product.isFavorite = !isFavorite;
+              },
+            );
+          }),
       title: Text(
         product.title,
         textAlign: TextAlign.center,
       ),
       trailing: IconButton(
-        icon: const Icon(Icons.shopping_cart),
+        icon: const Icon(
+          Icons.shopping_cart,
+        ),
         onPressed: () {
-          print('Add item to cart');
+          print('add item to cart');
         },
         color: Theme.of(context).colorScheme.secondary,
       ),
